ec7a15502d8174bf4f5203ffdd370de0bf0af83e
aqeeladil <aqeel.adil17@gmail.com>
2024-04-13 02:22:10 +0500

    api handling

---
+++ fetch_userData.py
@@ -0,0 +1,33 @@
+# This program fetches random username and country data from freeapi.app
+
+
+import requests
+
+
+def fetch_userdata():
+    url = "https://api.freeapi.app/api/v1/public/randomusers/user/random"
+    response = requests.get(url)
+    data = response.json()
+
+    if data["success"] and "data" in data:
+        user_data = data["data"]
+        username = user_data["login"]["username"]
+        country = user_data["location"]["country"]
+        return username, country
+    else:
+        raise Exception("Failed to fetch user data")
+    
+
+def main():
+    try:
+        username, country = fetch_userdata()
+        print(f"Username: {username} \nCountry: {country}")
+    except Exception as e:
+        print(str(e))
+
+
+if __name__ == "__main__":
+    main()
+
+
+
